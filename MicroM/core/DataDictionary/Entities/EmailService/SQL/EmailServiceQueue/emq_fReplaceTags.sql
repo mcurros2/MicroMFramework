30d86f56ab0d82262f9a0e90b0dd6f6e449e3d4d
mcurros2 <mcurros@gmail.com>
2025-05-02 11:20:05 -0300

    FIX replace tags to not throw errors and replace any unreplaced tag

--- MicroM/core/DataDictionary/Entities/EmailService/SQL/EmailServiceQueue/emq_fReplaceTags.sql
+++ MicroM/core/DataDictionary/Entities/EmailService/SQL/EmailServiceQueue/emq_fReplaceTags.sql
@@ -1,36 +1,33 @@
 ﻿create or alter function emq_fReplaceTags(@template varchar(max), @tags varchar(max)) returns nvarchar(max) as
 begin
-    if ISJSON(@tags) <> 1
-    begin
-        -- force error as throw is not allowed in functions
-        return cast('Invalid JSON format' as int);
-    end
-
-    -- Expected format
-    if exists (
-        select 1 
-        from openjson(@tags)
-        with (
-            tag varchar(255),
-            value varchar(255)
-        )
-        where tag is null or value is null
-    )
-    begin
-        -- force error as throw is not allowed in functions
-        return cast('Invalid JSON format. Expected [{tag: string, value: string}, ...]' as int);
-    end
-
     declare @FinalResult varchar(max) = @template;
 
     declare @Replacements table (tag varchar(255), value varchar(255));
-    insert into @Replacements(tag, value)
-    select tag, value
-    from openjson(@tags) 
-    with (
-        tag varchar(255),
-        value varchar(255)
-    );
+
+    -- If it's json and in expected format
+    if  ISJSON(@tags) = 1 
+        and
+        not exists 
+        (
+            select 1 
+            from openjson(@tags)
+            with 
+            (
+                tag varchar(255),
+                value varchar(255)
+            )
+            where tag is null
+        )
+
+    begin
+        insert into @Replacements(tag, value)
+        select tag, value
+        from openjson(@tags) 
+        with (
+            tag varchar(255),
+            value varchar(255)
+        );
+    end
 
     declare @CurrentTag varchar(255);
     declare @CurrentValue varchar(255);
@@ -44,5 +41,21 @@ begin
         delete from @Replacements where tag = @CurrentTag;
     end
 
+    -- Remove any empty tags in the format {TAG}. As SQL has no simple pattern matchijng for this, we remove anything between { and }
+    declare @EmptyTagPattern varchar(255) = '{%}'
+    
+    while patindex('%' + @EmptyTagPattern + '%', @FinalResult) > 0
+    begin
+        declare @EmptyTag varchar(255);
+        set @EmptyTag = substring(@FinalResult, patindex('%' + @EmptyTagPattern + '%', @FinalResult), 
+                                  charindex('}', @FinalResult + '}', patindex('%' + @EmptyTagPattern + '%', @FinalResult)) 
+                                  - patindex('%' + @EmptyTagPattern + '%', @FinalResult) + 1);
+
+        -- Ensure the tag is not in the replacements table
+        if not exists (select 1 from @Replacements where tag = @EmptyTag)
+        begin
+            set @FinalResult = replace(@FinalResult, @EmptyTag, '');
+        end
+    end
     return @FinalResult
 end
