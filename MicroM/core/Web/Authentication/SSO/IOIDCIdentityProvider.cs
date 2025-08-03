b300edc1370afeeb3ca760ae01e311baf00b1583
mcurros2 <mcurros@gmail.com>
2025-08-03 11:19:10 -0300

    Inprogress: SSO, OIDC

---
+++ MicroM/core/Web/Authentication/SSO/IOIDCIdentityProvider.cs
@@ -0,0 +1,15 @@
+﻿using MicroM.Configuration;
+
+namespace MicroM.Web.Authentication.SSO;
+
+public interface IOIDCIdentityProvider
+{
+    Task<Dictionary<string, string>> GetWellKnown(ApplicationOption app_config, CancellationToken ct);
+
+    Task<string> GetJwks(CancellationToken ct);
+
+    Task<string> GetAuthorizationCode(ApplicationOption app_config, string user_id, CancellationToken ct);
+
+    Task<string> GetBackchannelToken(ApplicationOption app_config, string user_id, CancellationToken ct);
+
+}
