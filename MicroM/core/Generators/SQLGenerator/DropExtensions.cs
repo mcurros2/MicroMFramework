5c73bb255cb99dd151521bd5afa8b3ec28810e57
mcurros2 <85686403+mcurros2@users.noreply.github.com>
2025-08-25 08:26:31 -0300

    Document drop SQL generator

--- MicroM/core/Generators/SQLGenerator/DropExtensions.cs
+++ MicroM/core/Generators/SQLGenerator/DropExtensions.cs
@@ -6,8 +6,19 @@ using static MicroM.Generators.Constants;
 
 namespace MicroM.Generators.SQLGenerator
 {
+    /// <summary>
+    /// Extension methods that generate SQL scripts for drop stored procedures.
+    /// </summary>
     internal static class DropExtensions
     {
+        /// <summary>
+        /// Builds scripts for an internal drop procedure and its caller for the specified entity.
+        /// </summary>
+        /// <typeparam name="T">Type of entity to drop.</typeparam>
+        /// <param name="entity">Entity definition used to generate the scripts.</param>
+        /// <param name="create_or_alter">Whether to emit a CREATE or CREATE OR ALTER statement.</param>
+        /// <param name="force_fake">Generate scripts even for entities marked as fake.</param>
+        /// <returns>SQL scripts for the internal drop procedure and wrapper.</returns>
         internal static List<string> AsCreateIDropProc<T>(this T entity, bool create_or_alter = false, bool force_fake = false) where T : EntityBase
         {
             List<string> scripts = [];
@@ -29,6 +40,14 @@ namespace MicroM.Generators.SQLGenerator
             return scripts;
         }
 
+        /// <summary>
+        /// Builds scripts for a standard drop procedure for the specified entity.
+        /// </summary>
+        /// <typeparam name="T">Type of entity to drop.</typeparam>
+        /// <param name="entity">Entity definition used to generate the scripts.</param>
+        /// <param name="create_or_alter">Whether to emit a CREATE or CREATE OR ALTER statement.</param>
+        /// <param name="force_fake">Generate scripts even for entities marked as fake.</param>
+        /// <returns>SQL scripts for the drop procedure.</returns>
         internal static List<string> AsCreateNormalDropProc<T>(this T entity, bool create_or_alter = false, bool force_fake = false) where T : EntityBase
         {
             List<string> scripts = [];
@@ -50,6 +69,15 @@ namespace MicroM.Generators.SQLGenerator
             return scripts;
         }
 
+        /// <summary>
+        /// Builds drop procedure scripts, optionally generating the internal drop variant.
+        /// </summary>
+        /// <typeparam name="T">Type of entity to drop.</typeparam>
+        /// <param name="entity">Entity definition used to generate the scripts.</param>
+        /// <param name="create_or_alter">Whether to emit a CREATE or CREATE OR ALTER statement.</param>
+        /// <param name="with_idrop">Generate internal drop scripts if set to <c>true</c>.</param>
+        /// <param name="force_fake">Generate scripts even for entities marked as fake.</param>
+        /// <returns>SQL scripts for the drop procedure.</returns>
         public static List<string> AsCreateDropProc<T>(this T entity, bool create_or_alter = false, bool with_idrop = false, bool force_fake = false) where T : EntityBase
         {
             if (with_idrop)
