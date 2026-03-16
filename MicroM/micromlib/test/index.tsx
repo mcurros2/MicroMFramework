79f1980457379ea6a12f6dbfbc750edb9221e8cc
Kirill Borounov <kirill.borounov@uai.edu.ar>
2026-03-16 18:34:16 -0300

    Fix test: faltan imports estilos

--- MicroM/micromlib/test/index.tsx
+++ MicroM/micromlib/test/index.tsx
@@ -1,3 +1,8 @@
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/dropzone/styles.css";
+import "@mantine/spotlight/styles.css";
+
 import { createRoot } from "react-dom/client"
 import { App } from "./App"
 import { StrictMode } from "react"
