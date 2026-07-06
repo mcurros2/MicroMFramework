00235c338cbf1f2f6ea6d658f32a3d65d5c8c602
Curros, Mariano <mcurros@gmail.com>
2026-07-06 16:52:38 -0300

    FIX hardcoded values

---
+++ MicroM/core/Extensions/ChannelExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace MicroM.Extensions;
+
+public static class ChannelExtensions
+{
+    public static async IAsyncEnumerable<object?[]> StreamRows(this ChannelReader<object?[]> rows, [EnumeratorCancellation] CancellationToken ct)
+    {
+        await foreach (var row in rows.ReadAllAsync(ct))
+        {
+            yield return row;
+        }
+    }
+}
